5d5121c3c92a4eae32153444b2dec1751f9e0718
Marc <flymia@hotmail.de>
2018-04-29 22:47:26 +0200

    Kamerapatch
    Eigenes Skript angefertigt

---
+++ Assets/Scripts/PlayerFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour {
+
+    public Transform PlayerTransform;
+    public Vector3 offset;
+    
+    [Range(0.01f, 1.0f)]
+    public float sFactor = 0.5f;
+
+    public bool lookAtPlayer = false;
+
+    // Use this for initialization
+    void Start () {
+        offset = transform.position - PlayerTransform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 neuePos = PlayerTransform.position + offset;
+
+        transform.position = Vector3.Slerp(transform.position, neuePos, sFactor);
+
+        if (lookAtPlayer)
+            transform.LookAt(PlayerTransform);
+
+	}
+}
